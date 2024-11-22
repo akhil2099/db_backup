20e3e1b2d48b01be3cd3f10601fe42a4bb285d36
akhil2099 <akhilvimal7@gmail.com>
2024-11-22 12:56:58 +0530

    script changes

--- scripts/backup_script.sh
+++ scripts/backup_script.sh
@@ -46,3 +46,13 @@ gzip $BACKUP_FILENAME 2>> $LOG_FILE
 # Log completion time
 echo "Backup completed at $(date)" >> $LOG_FILE
 
+
+
+#################################################################
+
+# Find and delete files older than 5 hours (300 minutes)
+find $BACKUP_DIR -type f -name "*.sql.gz" -mmin +300 -exec rm -f {} \;
+
+# Optional: Log the deletion
+echo "Cleanup completed at $(date)" >> /home/akhil/db-backup/logs/cleanup_log.txt
+
